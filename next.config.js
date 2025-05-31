a0dbda3552efa173794750457429e2b808b0f881
Alexandr Xoxon <4218820@gmail.com>
2025-05-31 17:35:19 +0300

    dobavil-ngrok

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
-const os = require('os');
-
-function getLocalIp() {
-  const nets = os.networkInterfaces();
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      // ищем IPv4, не internal (не локальный loopback)
-      if (net.family === 'IPv4' && !net.internal) {
-        return net.address;
-      }
-    }
-  }
-  return 'localhost'; // если не нашли — fallback
-}
-
-const localIp = getLocalIp();
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [`http://${localIp}:3000`],
-  },
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: 'https://7d3a-45-38-170-113.ngrok-free.app/:path*',
+    },
+  ],
 };
 
 module.exports = nextConfig;
